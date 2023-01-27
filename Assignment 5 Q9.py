08a350b6c7f8bbd82c3714d872209e2b9486954d
Hemantkumar7803 <121182169+Hemantkumar7803@users.noreply.github.com>
2023-01-27 23:44:18 +0530

    Create Assignment 5 Q9.py

---
+++ Assignment 5 Q9.py
@@ -0,0 +1,13 @@
+# assignment-5
+def counter(a,i):
+    n=0
+    n=a.count(i)
+    return n
+a=[]
+x=int(input("enter no. of words to be entered in list="))
+for i in range(0,x):
+    y=input("enter word=")
+    a.append(y)
+print(a)
+for i in a:
+  print(f"{i} occurs {counter(a,i)} times")
